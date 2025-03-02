a5f3beb4130ffcf880b218e4f5de12ae3a7c0a2a
timothyobosi <timothyobosi8@gmail.com>
2025-03-02 20:43:28 +0300

    Header name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <AppBar position="static">
             <Toolbar className={classes.toolbar}>
                 <Typography variant="h5" className={classes.title}>
-                    Travel Advisor
+                    Intrepid DMC
                 </Typography>
                 <Box display="flex">
                     <Typography variant="h6" className={classes.title}>
