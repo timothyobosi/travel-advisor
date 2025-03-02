a204a812c1b112ba17e29192200743d38920cc49
timothyobosi <timothyobosi8@gmail.com>
2025-03-02 17:37:57 +0300

    UI for list and Map

--- src/App.js
+++ src/App.js
@@ -1,43 +1,31 @@
 import React from 'react';
-
-//import stif from material UI
-import { CssBaseline, Grid} from '@mui/material';
+import { CssBaseline, Grid } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles'; // Add ThemeProvider and createTheme
 
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
+// Create a default theme
+const theme = createTheme();
 
-
-const App = () =>{
-    return(
-        // turn to react fragment and call as self closing css baseline components
-        //Grid of type item =2 properties
-        //s=12-Take full width on mobile devices
-        //md=4-Medium & Lager its goin to take only 4 spaces
-        //<List/> //self closing tag
-        //Grid contain Map- Larger than the list
-        
+const App = () => {
+    return (
         <>
-            <CssBaseline />
-            <Header />
-            <Grid container spacing={3} style={{width: '100%'}}>
-                
-                <Grid item xs={12} md={4}>
-                    <List/> 
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Header />
+                <Grid container spacing={3} style={{ width: '100%' }}>
+                    <Grid item xs={12} md={4}>
+                        <List />
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Map />
+                    </Grid>
                 </Grid>
-
-                
-                <Grid item xs = {12} md = {8}>
-                    <Map/>
-
-                </Grid>
-
-            </Grid>
-        
-
+            </ThemeProvider>
         </>
     );
-}
+};
 
 export default App;
